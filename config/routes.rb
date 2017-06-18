16e58abf2ee468bf5c5e3d73d83ca40296773d89
utcizumi masato <u.masato0808@gmail.com>
2017-06-18 22:57:28 +0900

    edit routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
   resources :staffs, only: [:new, :create, :edit, :update, :destroy]
+  resources :shifts, only: [:index, :new, :create, :edit, :update, :show]
+  resources :users, only: [:show]
 end
